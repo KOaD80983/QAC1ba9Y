2e842918cad69b96e81bc03470b61c00d96f3b3f
KOaD80983 <vibe.00magnet@icloud.com>
2025-08-11 01:45:26 +0900

    Develop new RESTful API接口开发 feature

---
+++ restful_api_0811_0045_cfg.go
@@ -0,0 +1,10 @@
+// 代码生成时间: 2025-08-11 00:45:25
+package main
+
+import (
+    "buffalo"
+    "github.com/gobuffalo/buffalo/generators"
+    "github.com/gobuffalo/buffalo/generators/assets/escmd"
+    "github.com/gobuffalo/buffalo/generators/assets/esmcmd"
+    "github.com/gobuffalo/buffalo/generators/assets/generator"
+    "github.com/markbates/refresh/refresh
